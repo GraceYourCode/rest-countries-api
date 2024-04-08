558efcdba59b9472281d3b639d896caf86696631
GraceYourCode <freshmannskuls@gmail.com>
2024-04-08 03:57:14 -0700

    more commit

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -3,6 +3,7 @@ import NavBar from "../components/NavBar"
 import Contents from "../components/Contents"
 import allData from "../store/index."
 import { useState } from "react"
+import { MdKeyboardBackspace } from "react-icons/md";
 
 const Details = () => {
   const [data, setData] = useState();
@@ -21,9 +22,12 @@ const Details = () => {
       <div className='dark:bg-dark-bg bg-light-bg font-primary min-h-screen flex flex-col gap-8 sm:gap-10'>
         <NavBar />
 
-        <Link to="/" className="bg-white dark:bg-dark-elements no-underline px-12 w-fit
+        <Link to="/" className="bg-white dark:bg-dark-elements no-underline px-9 w-fit
       ml-4 sm:ml-8 md:ml-16 lg:ml-20 py-2 text-sm shadow-md dark:shadow-none
-      text-light-text dark:text-white flex justify-between">Back</Link>
+      text-light-text dark:text-white flex gap-2 items-center">
+        <MdKeyboardBackspace />
+        <span>Back</span>
+      </Link>
 
         <Contents />
       </div>
